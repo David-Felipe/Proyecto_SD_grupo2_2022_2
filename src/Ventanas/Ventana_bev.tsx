2ada53a6535bdd67334d57df14da53805d0fe7a6
Juan Carreño <jcarrenoar@unal.edu.co>
2022-11-04 21:54:53 -0500

    Detalles en la interfaz

--- src/Ventanas/Ventana_bev.tsx
+++ src/Ventanas/Ventana_bev.tsx
@@ -25,7 +25,7 @@ export default class Ventana_bev extends React.Component<Props, {}> {
         {/* <!-- Aqui va el resto --> */}
         <div className="Content">
           <div>
-            <div className="mapa">
+            <div className="mapa_bev">
               {/* <!-- Aqui va implementado el mapa --> */}
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d15941.379358161692!2d-74.08466621092664!3d4.643676758242312!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sco!4v1666105674940!5m2!1ses-419!2sco"
