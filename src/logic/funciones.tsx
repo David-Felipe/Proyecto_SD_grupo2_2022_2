2258c4ad8df0608cef7aeea98104368c4c606077
laura0844 <115044955+laura0844@users.noreply.github.com>
2022-11-06 17:57:15 -0500

    Update funciones.tsx

--- src/logic/funciones.tsx
+++ src/logic/funciones.tsx
@@ -2,12 +2,16 @@
 //------------------------------------------Mini eventos------------------------------------------------------
 
 //Metodo de creacion de minieventos
-function crearMiniEvento(nombre : string, hora: number, tematica: string) {
+
+function crearMiniEvento( name: string,distancia: number,address: string,time_begin: Date,time_end: Date,thematics: string[]) {
 
     let miniEvento = new LinkedList();
-    miniEvento.pushBack(nombre);
-    miniEvento.pushBack(hora);
-    miniEvento.pushBack(tematica);
+    miniEvento.pushBack(name);
+    miniEvento.pushBack(distancia);
+    miniEvento.pushBack(address);
+    miniEvento.pushBack(time_begin);
+    miniEvento.pushBack(time_end);
+    miniEvento.pushBack(thematics);
     miniEvento.pushBack(heapMiniEventos.getSize());
   
     heapMiniEventos.insert(heapMiniEventos.getSize(), miniEvento);
